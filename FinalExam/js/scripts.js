6f5a75b0f1b871f829f1822c08a88e6c33bf1381
Samanta Fluture <samantafluture@gmail.com>
2022-08-03 14:08:38 -0400

    validate name

--- FinalExam/js/scripts.js
+++ FinalExam/js/scripts.js
@@ -1,3 +1,24 @@
+function validate_user() {
+	var userName = document.getElementById('name').value;
+
+	if (userName == '') {
+		alert('Visitor name must be filled out!');
+		return false;
+	} else {
+		if (!/^[ ]*[A-Z][a-z]([a-z]|(([ ]+)[A-Z]))*[ ]*$/.test(userName)) {
+			alert('Invalid name!');
+			return false;
+		} else {
+			document.getElementById('name').value = userName
+				.replace(/[ ][ ]+/g, ' ')
+				.replace(/(^[ ])|([ ]$)/g, '');
+
+			return true;
+		}
+	}
+}
+
+/*
 function validate() {
 	var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
@@ -29,3 +50,4 @@ function validate() {
 		}
 	}
 }
+*/
